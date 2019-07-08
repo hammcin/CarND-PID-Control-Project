2852d46a010144432c08b38572506eb27679e60e
Hamadi McIntosh <hamadi.mc@sbcglobal.net>
2019-07-08 16:49:43 -0400

    feat: Implement twiddle parameter optimization

--- src/PID.cpp
+++ src/PID.cpp
@@ -12,27 +12,27 @@ void PID::Init(double Kp_, double Ki_, double Kd_) {
   /**
    * TODO: Initialize PID coefficients (and errors, if needed)
    */
-   this->Kp = Kp_;
-   this->Ki = Ki_;
-   this->Kd = Kd_;
-   this->p_error = 0.0;
-   this->i_error = 0.0;
+  this->Kp = Kp_;
+  this->Ki = Ki_;
+  this->Kd = Kd_;
+  this->p_error = 0.0;
+  this->i_error = 0.0;
 }
 
 void PID::UpdateError(double cte) {
   /**
    * TODO: Update PID errors based on cte.
    */
-   this->d_error = cte - this->p_error;
-   this->i_error += cte;
-   this->p_error = cte;
+  this->d_error = cte - this->p_error;
+  this->i_error += cte;
+  this->p_error = cte;
 }
 
 double PID::TotalError() {
   /**
    * TODO: Calculate and return the total error
    */
-   double steer = -this->Kp*this->p_error - this->Kd*this->d_error
-                  - this->Ki*this->i_error;
+  double steer = -this->Kp*this->p_error - this->Kd*this->d_error
+                 - this->Ki*this->i_error;
   return steer;  // TODO: Add your total error calc here!
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,14 @@
 #include <uWS/uWS.h>
 #include <iostream>
 #include <string>
+#include <vector>
 #include "json.hpp"
 #include "PID.h"
 
 // for convenience
 using nlohmann::json;
 using std::string;
+using std::vector;
 
 // For converting back and forth between radians and degrees.
 constexpr double pi() { return M_PI; }
@@ -34,13 +36,36 @@ int main() {
   uWS::Hub h;
 
   PID pid;
+
+  bool initial = true;
+  bool twiddle = true;
+  int verbose = 3;
+  vector<double> p = {0.2, 0.004, 3.0}; // initial
+  vector<double> p = {0.18, 0.004, 3.0}; // after twiddle
+  vector<double> dp = {0.1, 0.001, 1.0};
+  double tol = 0.05;
+  double err = 0.0;
+  double mse = 0.0;
+  double best_err = 0.0;
+  double dp_sum = 0.0;
+  for (int i=0; (unsigned)i<dp.size(); ++i)
+  {
+    dp_sum += dp[i];
+  }
+  int count = 0;
+  int n = 100;
+  int p_index = 0;
+  int prev_p_index = p.size();
+
   /**
    * TODO: Initialize the pid variable.
    */
-   pid.Init(0.0, 0.0, 0.0);
+   pid.Init(p[0], p[1], p[2]);
 
-  h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
-                     uWS::OpCode opCode) {
+  h.onMessage([&pid, &initial, &twiddle, &verbose, &p, &dp, &tol, &err, &mse,
+               &best_err, &dp_sum, &count, &n, &p_index, &prev_p_index]
+              (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+               uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
@@ -55,8 +80,8 @@ int main() {
         if (event == "telemetry") {
           // j[1] is the data JSON object
           double cte = std::stod(j[1]["cte"].get<string>());
-          double speed = std::stod(j[1]["speed"].get<string>());
-          double angle = std::stod(j[1]["steering_angle"].get<string>());
+          // double speed = std::stod(j[1]["speed"].get<string>());
+          // double angle = std::stod(j[1]["steering_angle"].get<string>());
           double steer_value;
           /**
            * TODO: Calculate steering value here, remember the steering value is
@@ -64,18 +89,225 @@ int main() {
            * NOTE: Feel free to play around with the throttle and speed.
            *   Maybe use another PID controller to control the speed!
            */
-           pid.UpdateError(cte);
-           steer_value = pid.TotalError();
+           if (verbose == 1)
+           {
+             std::cout << "Initial: " << initial << std::endl;
+             std::cout << "Twiddle: " << twiddle << std::endl;
+           }
+
+           if (twiddle)
+           {
+             if (initial)
+             {
+               if (verbose == 1)
+               {
+                 std::cout << "Initial and Twiddle? " << (initial && twiddle) << std::endl;
+               }
+
+               pid.UpdateError(cte);
+               steer_value = pid.TotalError();
+
+               count += 1;
+               if (verbose == 1)
+               {
+                 std::cout << "Count: " << count << std::endl;
+               }
+
+               if (count > n)
+               {
+                 err += cte*cte;
+               }
+
+               if (count == 2*n)
+               {
+                 best_err = err/n;
+                 if (verbose == 2)
+                 {
+                   std::cout << "err: " << err << std::endl;
+                   std::cout << "n: " << n << std::endl;
+                   std::cout << "Initial error: " << best_err << std::endl;
+                 }
+                 initial = false;
+
+                 err = 0.0;
+                 count = 0;
+
+               }
+             }
+             else
+             {
+
+               if (verbose == 1)
+               {
+                 std::cout << "Begin Twiddle"  << std::endl;
+                 std::cout << "twiddle: " << twiddle << std::endl;
+                 std::cout << "initial: " << initial << std::endl;
+               }
+
+               if (dp_sum > tol)
+               {
+                 if (count == 0)
+                 {
+                   if (p_index == prev_p_index)
+                   {
+                     p[p_index] -= 2*dp[p_index];
+                   }
+                   else
+                   {
+                     p[p_index] += dp[p_index];
+                   }
+
+
+                   if (verbose == 3)
+                   {
+                     std::cout << "P: ";
+                     for (int i=0; (unsigned)i<p.size(); ++i)
+                     {
+                       std::cout << p[i] << " ";
+                     }
+                     std::cout << std::endl;
+                   }
+
+
+                   pid.Init(p[0], p[1], p[2]);
+                 }
+
+                 if (verbose == 2)
+                 {
+                   std::cout << "Count: " << count << std::endl;
+                   std::cout << "P index: " << p_index << std::endl;
+                 }
+
+                 pid.UpdateError(cte);
+                 steer_value = pid.TotalError();
+
+                 count += 1;
+
+                 if (count > n)
+                 {
+                   err += cte*cte;
+                 }
+
+                 if (count == 2*n)
+                 {
+                   mse = err/n;
+
+
+                   if (verbose == 3)
+                   {
+                     std::cout << "MSE: " << mse << std::endl;
+                   }
+
+
+                   if (mse < best_err)
+                   {
+                     best_err = mse;
+                     if (verbose == 3)
+                     {
+                       std::cout << "New Best Error: " << best_err << std::endl;
+                     }
+
+                     dp[p_index] *= 1.1;
+
+                     prev_p_index = p.size();
+
+                     p_index += 1;
+                     if ((unsigned)p_index == p.size())
+                     {
+                       p_index = 0;
+                     }
+                   }
+                   else
+                   {
+                     if (p_index == prev_p_index)
+                     {
+                       p[p_index] += dp[p_index];
+
+                       if (verbose == 3)
+                       {
+                         std::cout << "P: ";
+                         for (int i=0; (unsigned)i<p.size(); ++i)
+                         {
+                           std::cout << p[i] << " ";
+                         }
+                         std::cout << std::endl;
+                       }
+
+                       dp[p_index] *= 0.9;
+
+                       prev_p_index = p.size();
+
+                       p_index += 1;
+                       if ((unsigned)p_index == p.size())
+                       {
+                         p_index = 0;
+                       }
+                     }
+                     else
+                     {
+                       prev_p_index = p_index;
+                     }
+                   }
+
+                   count = 0;
+                   err = 0.0;
+                 }
+
+                 if (verbose == 2)
+                 {
+                   std::cout << "Count: " << count << std::endl;
+                   std::cout << "P index: " << p_index << std::endl;
+                   std::cout << "Previous p index: " << prev_p_index << std::endl;
+                   std::cout << "Test: " << (count==0 && p_index==0 && p_index!=prev_p_index) << std::endl;
+                 }
+
+                 if (count == 0 && p_index == 0 && p_index != prev_p_index)
+                 {
+                   dp_sum = 0.0;
+                   for (int i=0; (unsigned)i<dp.size(); ++i)
+                   {
+                     dp_sum += dp[i];
+                   }
+
+                   if (verbose == 2)
+                   {
+                     std::cout << "Sum of dp: " << dp_sum << std::endl;
+                   }
+                 }
+
+               }
+               else
+               {
+                 pid.UpdateError(cte);
+                 steer_value = pid.TotalError();
+               }
+             }
+
+           }
+           else
+           {
+             if (verbose == 2)
+             {
+               std::cout << "No Twiddle"  << std::endl;
+               std::cout << "twiddle: " << twiddle << std::endl;
+               std::cout << "initial: " << initial << std::endl;
+             }
+
+             pid.UpdateError(cte);
+             steer_value = pid.TotalError();
+           }
 
           // DEBUG
+          /*
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value
                     << std::endl;
+          */
 
           json msgJson;
           msgJson["steering_angle"] = steer_value;
           msgJson["throttle"] = 0.3;
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
-          std::cout << msg << std::endl;
+          // std::cout << msg << std::endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
         }  // end "telemetry" if
       } else {
